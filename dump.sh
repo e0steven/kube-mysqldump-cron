50ffe7b234ae739e391c9c9babd88dd8f3c90e62
e0steven <e0steven@gmail.com>
2019-01-31 15:27:21 -0500

    fixing script removing loops

--- dump.sh
+++ dump.sh
@@ -33,10 +33,7 @@ else
 for db in $databases; do
     if [[ "$db" != "information_schema" ]] && [[ "$db" != "performance_schema" ]] && [[ "$db" != "mysql" ]] && [[ "$db" != "sys" ]] && [[ "$db" != _* ]] && [[ "$db" != "$IGNORE_DATABASE" ]]; then
         echo "Dumping database: $db"
-        mysqldump --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" --databases $db > /mysqldump/$db.sql
+        mysqldump --user="${DB_USER}" --password="${DB_PASS}" --host="${DB_HOST}" --databases $db > /mysqldump/"$db-${DATESTAMP}".sql
     fi
 done	
 fi
-while true; do 
-	sleep 30
-done 
